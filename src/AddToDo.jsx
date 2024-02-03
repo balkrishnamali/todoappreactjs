a9c8c0a1c841989ff4428be5e270836e3a712fd2
balkrishnamali <balkrishna_mali@rediffmail.com>
2024-02-03 10:40:05 +0530

    '#Test commit"

--- src/AddToDo.jsx
+++ src/AddToDo.jsx
@@ -1,39 +1,32 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import { IoMdAddCircle } from "react-icons/io";
 
-export default function AddToDo(props) {
-  const [item, setItem] = useState();
-  const [date, setDate] = useState();
+export default function AddToDo({ setTodoDataInArray }) {
+  const inputName = useRef();
+  const inputDate = useRef();
+
+  function handleClick() {
+    const newName = inputName.current.value;
+    const newDate = inputDate.current.value;
+    inputName.current.value = "";
+    inputDate.current.value = "";
+    setTodoDataInArray(newName, newDate);
+  }
 
   return (
     <div className="container text-center">
       <div className="row">
         <div className="col-6 paddingTop">
-          <input
-            type="text"
-            placeholder="input todo here"
-            value={item}
-            onChange={(e) => setItem(e.target.value)}
-          />
+          <input type="text" placeholder="input todo here" ref={inputName} />
         </div>
         <div className="col-4 paddingTop">
-          <input
-            type="date"
-            value={date}
-            onChange={(e) => {
-              setDate(e.target.value);
-            }}
-          />
+          <input type="date" ref={inputDate} />
         </div>
         <div className="col-2">
           <button
             type="button"
             className="btn btn-success"
-            onClick={() => {
-              props.setTodoDataInArray(item, date);
-              setItem("");
-              setDate("");
-            }}
+            onClick={handleClick}
           >
             <IoMdAddCircle />
           </button>
